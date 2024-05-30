c8a7e03ed84eb9322f1179e0760107ca0c39cae3
vattis <superhsk00@gmail.com>
2024-05-31 01:09:26 +0900

    .

--- src/main/java/com/example/demo/login/jwt/JwtGenerator.java
+++ src/main/java/com/example/demo/login/jwt/JwtGenerator.java
@@ -7,13 +7,55 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+import static com.cheering.auth.constant.JwtConstant.ACCESS_TOKEN_EXPIRE_TIME;
+import static com.cheering.auth.constant.JwtConstant.GRANT_TYPE;
+import static com.cheering.auth.constant.JwtConstant.REFRESH_TOKEN_EXPIRE_TIME;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 @Component
 public class JwtGenerator {
     private final Key key;
-    public JwtGenerator(@Value("@{jwt.secret}")String secretKey){
+
+    // application.yml에서 secret 값 가져와서 key에 저장
+    public JwtGenerator(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
+    // Member 정보를 가지고 AccessToken, RefreshToken을 생성하는 메서드
+    public JwtToken generateToken(Long userId) {
+        // 권한 가져오기
 
+        long now = (new Date()).getTime();
+
+        // Access Token 생성
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+
+        String accessToken = Jwts.builder()
+                .setSubject(String.valueOf(userId))
+//                .claim("auth", authorities)
+                .setExpiration(accessTokenExpiresIn)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        // Refresh Token 생성
+        String refreshToken = Jwts.builder()
+                .setExpiration(new Date(now + REFRESH_TOKEN_EXPIRE_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return JwtToken.builder()
+                .grantType(GRANT_TYPE)
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
 }
