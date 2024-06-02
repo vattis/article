76ae463e2643ecac2545ffd03690a00fc53b3bde
vattis <superhsk00@gmail.com>
2024-06-03 00:01:27 +0900

    .

--- src/main/java/com/example/demo/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/filter/JwtAuthenticationFilter.java
@@ -2,44 +2,47 @@ package com.example.demo.filter;
 
 import com.example.demo.login.jwt.JwtConstant;
 import com.example.demo.login.jwt.JwtProvider;
+import com.example.demo.login.jwt.JwtUtil;
+import com.example.demo.member.domain.Member;
+import com.example.demo.member.service.CustomMemberDetailService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends GenericFilterBean {
-    private final JwtProvider jwtTokenProvider;
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final CustomMemberDetailService customMemberDetailService;
+    private final JwtUtil jwtUtil;
 
-    @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        // 1. Request Header에서 JWT 토큰 추출
-        String token = resolveToken((HttpServletRequest) request);
-        // 2. validateToken으로 토큰 유효성 검사
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            // 토큰이 유효할 경우 토큰에서 Authentication 객체를 가지고 와서 SecurityContext에 저장
-            Authentication authentication = jwtTokenProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            chain.doFilter(request, response);
-        }
-        // 에러 핸들링 필요
-        chain.doFilter(request, response);
-    }
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
 
-    // Request Header에서 토큰 정보 추출
-    private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtConstant.GRANT_TYPE)) {
-            return bearerToken.substring(7);
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            String token = authorizationHeader.substring(7);
+            if(jwtUtil.validateToken(token)){
+                Long id = jwtUtil.getMemberId(token);
+
+                UserDetails userDetails = customMemberDetailService.loadUserByUsername(id.toString());
+                if(userDetails != null){
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                }
+            }
         }
-        return null;
+        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/example/demo/login/jwt/JwtUtil.java
+++ src/main/java/com/example/demo/login/jwt/JwtUtil.java
@@ -1,9 +1,10 @@
 package com.example.demo.login.jwt;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.example.demo.member.domain.CustomMemberInfoDto;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,14 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 @Slf4j
 @Component
 public class JwtUtil {
     private final Key key;
-    private final long accessTokenTime;
+    private final long accessTokenExpTime;
 
     public JwtUtil(@Value("${spring.jwt.secret}") String secretKey, @Value("${spring.jwt.expiration_time}") long accessTokenExpTime){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
@@ -26,5 +28,49 @@ public class JwtUtil {
         this.accessTokenTime = accessTokenExpTime;
     }
 
-    public String createAccessToken()
+    public String createAccessToken(CustomMemberInfoDto member){
+        return create
+    }
+
+    private String createToken(CustomMemberInfoDto member, long expireTime){
+        Claims claims = Jwts.claims();
+        claims.put("id", member.getId());
+        claims.put("memberId", member.getMemberId());
+        claims.put("role", member.getRole());
+
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime tokenValidity = now.plusSeconds(expireTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(Date.from(now.toInstant()))
+                .setExpiration(Date.from(tokenValidity.toInstant()))
+                .signWith(key, SignatureAlgorithm.ES256)
+                .compact();
+    }
+    public Long getMemberId(String token){
+        return parseClaims(token).get("memberId", Long.class);
+    }
+
+    public boolean validateToken(String token){
+        try{
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        }catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e){
+            log.info("Expire JWT Token", e);
+        }catch(UnsupportedJwtException e){
+            log.info("Unsupported JWT token", e);
+        }catch(IllegalArgumentException e){
+            log.info("JWT claims string is empty", e);
+        }
+        return false;
+    }
+
+    public Claims parseClaims(String accessToken){
+        try{
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        }catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
+    }
 }
